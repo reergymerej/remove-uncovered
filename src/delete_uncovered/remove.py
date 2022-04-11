a008aec36cf65aefefe2fab7efec461a772d4cce
Jeremy Greer <jex.grizzle@gmail.com>
2022-04-11 13:10:37 -0400

    Note last run and prevent running again when cov is unchanged

--- src/delete_uncovered/remove.py
+++ src/delete_uncovered/remove.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+import sys
+import pathlib
+from tkinter import W
 from typing import List, Tuple
 import re
 
@@ -40,11 +44,16 @@ def get_leading_space(line: str) -> str:
 
 def remove_uncovered(uncovered: List[Uncovered]):
     for filename, line_numbers in uncovered:
-        with open(filename, "r", encoding="utf8") as file:
+
+        # fixed_filename = f"src/{filename}"
+        fixed_filename = filename
+        # if "integration" not in fixed_filename:
+        #     continue
+        print(fixed_filename)
+        with open(fixed_filename, "r", encoding="utf8") as file:
             lines = file.readlines()
             for n in line_numbers[::-1]:
                 line_number_index = n - 1
-
                 # use slice trick to insert a return right before
                 original_line = lines[line_number_index]
                 leading_space = get_leading_space(original_line)
@@ -52,12 +61,56 @@ def remove_uncovered(uncovered: List[Uncovered]):
                 lines[line_number_index] = f"# {original_line}"
                 # f"{leading_space}return\n",
                 # original_line,
-
         # print("".join(lines))
         with open(filename, "w", encoding="utf8") as file:
             file.writelines(lines)
 
 
-tree = ET.parse("./coverage.xml")
-uncovered = get_uncovered(tree)
-remove_uncovered(uncovered)
+def time_to_int():
+    pass
+
+
+def get_last_run() -> int:
+    last_run_path = pathlib.Path("./last_run")
+    try:
+        with open(last_run_path, "r") as f:
+            text = f.read()
+            if text == "":
+                return None
+            return int(text)
+    except FileNotFoundError:
+        return None
+
+
+def set_last_run(value: int) -> None:
+    last_run_path = pathlib.Path("./last_run")
+    with open(last_run_path, "w") as f:
+        f.write(str(value))
+
+
+def get_run_time_from_coverage_file(coverage_file_path: pathlib.Path) -> int:
+    if coverage_file_path.exists():
+        modifed = coverage_file_path.stat().st_mtime
+        return int(modifed)
+    else:
+        raise Exception("Why is this running without a coverage file?")
+
+
+previous_coverage_ts = get_last_run()
+
+coverage_file_path = pathlib.Path("./coverage.xml")
+coverage_timestamp = get_run_time_from_coverage_file(coverage_file_path)
+should_run = previous_coverage_ts is None or previous_coverage_ts < coverage_timestamp
+
+
+if should_run:
+    tree = ET.parse(coverage_file_path)
+    uncovered = get_uncovered(tree)
+    remove_uncovered(uncovered)
+    set_last_run(coverage_timestamp)
+else:
+    # print(
+    #     f"previous_coverage_ts {previous_coverage_ts}, coverage_timestamp {coverage_timestamp}"
+    # )
+    print("no coverage changes")
+    sys.exit(1)
